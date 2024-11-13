1615ec986506e0433ea6dba9935153787c8413b3
EverlastingYuki <2063331724@qq.com>
2024-11-13 17:39:32 +0800

    feat(back_end): 支持新的多图片同时预测
    - 更新 API 文档，使其更加详细和规范
    - 优化预测功能，支持多图像输入
    - 重新组织工作区相关 API
    - 修复静态文件发送功能
    - 优化图像上传接口

--- back_end/api/predict.py
+++ back_end/api/predict.py
@@ -20,24 +20,38 @@ import yaml
 from back_end.util.clear_results import clear_results
 from back_end.util.post_process import post_process_image, process_image, deeplab_postprocess
 
+STATIC_DIR = os.path.abspath(os.path.join(os.getcwd(), 'front_end/static'))
+
 
 @api.route("start", methods=["POST"])
 def start():
     """
     开始预测接口，根据请求中的模型列表调用相应的预测函数，并返回预测结果的路径。
 
-    ### args
-    |  args          | required | request type | type |  remarks                  |
-    |----------------|----------|--------------|------|---------------------------|
-    | image_url      |  true    |    json      | str  | 图像文件路径  |
-    | models         |  true    |    json      | list | 需要使用的模型列表  |
+    ### 参数
+    | 参数名       | 必填 | 请求类型 | 类型   | 说明                           |
+    |--------------|------|----------|--------|--------------------------------|
+    | image_url    | 是   | json     | list   | 图像文件路径                   |
+    | models       | 是   | json     | list   | 需要使用的模型列表             |
 
-    ### request
+    ### 请求示例
     ```
 json
-    {"image_url": "", "models": ["U-net", "DeepLab", "WeClip"]}
+    {
+        "image_url": [
+            "http://localhost:5000/api/static/uploaded/three_channel/ID_0188_Z_0137.png",
+            "http://localhost:5000/api/static/uploaded/three_channel/ID_0189_Z_0132.png",
+            "http://localhost:5000/api/static/uploaded/three_channel/ID_0190_Z_0070.png",
+            "http://localhost:5000/api/static/uploaded/three_channel/ID_0191_Z_0140.png"
+        ],
+        "models": [
+            "U-net",
+            "DeepLab",
+            "WeClip"
+        ]
+    }
     ```
-    ### return
+    ### 返回示例
     ```
 json
     {
@@ -60,98 +74,67 @@ json
     }
     ```
     - 返回一个 JSON 对象，包含原始图像路径和每个模型的预测结果路径。
+
     """
     data = request.json
-    image_path = data.get('image_url')
+    image_urls = data.get('image_url')
     models = data.get('models')
+
     clear_results()
+    temp_dir = os.path.join(STATIC_DIR, 'temp')
+    shutil.rmtree(temp_dir)
+
+    one_channel_temp_dir = os.path.join(temp_dir, 'one_channel')
+    three_channel_temp_dir = os.path.join(temp_dir, 'three_channel')
+
+    os.makedirs(one_channel_temp_dir, exist_ok=True)
+    os.makedirs(three_channel_temp_dir, exist_ok=True)
+
+    # 复制图片到临时文件夹
+    for url in image_urls:
+        image_path = url.split('/')[-1]
+        print('Copying image:', image_path)
+        original_one_channel = os.path.join(ONE_CHANNEL_DIR, image_path)
+        original_three_channel = os.path.join(THREE_CHANNEL_DIR, image_path)
+        shutil.copy(original_one_channel, one_channel_temp_dir)
+        shutil.copy(original_three_channel, three_channel_temp_dir)
+
+    # 调用预测函数
     if 'U-net' in models:
-        predict_Unet(PROJECT_ROOT, ONE_CHANNEL_DIR, THREE_CHANNEL_DIR, UNET_DIR)
+        predict_Unet(PROJECT_ROOT, one_channel_temp_dir, three_channel_temp_dir, UNET_DIR)
     if 'DeepLab' in models:
-        predict_deeplab(PROJECT_ROOT, ONE_CHANNEL_DIR, THREE_CHANNEL_DIR, DEEPLAB_DIR)
+        predict_deeplab(PROJECT_ROOT, one_channel_temp_dir, three_channel_temp_dir, DEEPLAB_DIR)
         # deeplab_postprocess()
     if 'WeClip' in models:
-        predict_WeClip(PROJECT_ROOT, ONE_CHANNEL_DIR, THREE_CHANNEL_DIR, UNET_DIR)
-    process_image()
+        predict_WeClip(PROJECT_ROOT, one_channel_temp_dir, three_channel_temp_dir, UNET_DIR)
+    process_image(three_channel_temp_dir)
+    shutil.rmtree(temp_dir)
+
     response_data = {}
     for i in os.listdir(OVERLAY_DIR):
-        response_data[i] = [f'./static/result/overlay/{i}/{j}' for j in os.listdir(os.path.join(OVERLAY_DIR, i))]
+        response_data[i] = [f'http://localhost:5000/api/static/result/overlay/{i}/{j}' for j in
+                            os.listdir(os.path.join(OVERLAY_DIR, i))]
+
     return jsonify(response_data)
-
-
-@api.route('static/result/<model_name>/<path:filename>', methods=['GET'])
-def send_image(model_name, filename):
-    """
-    发送图像，根据请求中的模型名称和文件名返回相应的图像文件。
-
-    ### args
-    |  args          | required | request type | type |  remarks                  |
-    |----------------|----------|--------------|------|---------------------------|
-    | model_name     |  true    |    path      | str  | 模型名称  |
-    | filename       |  true    |    path      | str  | 文件名  |
-
-    ### request
-    - GET 请求，URL 中包含模型名称和文件名。
-
-    ### return
-    - 返回指定模型的预测结果图像文件。
-    - 如果模型名称无效，返回错误信息和状态码 404。
-    """
-    if model_name == 'U-net':
-        return send_from_directory(UNET_DIR, filename)
-    elif model_name == 'DeepLab':
-        return send_from_directory(DEEPLAB_DIR, filename)
-    elif model_name == 'WeClip':
-        return send_from_directory(WECLIP_DIR, filename)
-    else:
-        return "Invalid model name", 404
-
-
-# @api.route('static/result/<model_name>/<path:filename>', methods=['GET'])
-# def send_image(model_name, filename):
-#     """
-#     发送图像，根据请求中的模型名称和文件名返回相应的图像文件。
-#
-#     ### args
-#     |  args          | required | request type | type |  remarks                  |
-#     |----------------|----------|--------------|------|---------------------------|
-#     | model_name     |  true    |    path      | str  | 模型名称  |
-#     | filename       |  true    |    path      | str  | 文件名  |
-#
-#     ### request
-#     - GET 请求，URL 中包含模型名称和文件名。
-#
-#     ### return
-#     - 返回指定模型的预测结果图像文件。
-#     - 如果模型名称无效，返回错误信息和状态码 404。
-#     """
-#     if model_name == 'U-net':
-#         return send_from_directory(UNET_DIR, filename)
-#     elif model_name == 'DeepLab':
-#         return send_from_directory(DEEPLAB_DIR, filename)
-#     elif model_name == 'WeClip':
-#         return send_from_directory(WECLIP_DIR, filename)
-#     else:
-#         return "Invalid model name", 404
-
-
-STATIC_DIR = os.path.abspath(os.path.join(os.getcwd(), 'front_end/static'))
-
-
-# @api.route('/static/<path:filename>')
-# def serve_static(filename):
-#     print("发送图片: " + filename)
-#
-#     # 动态确定文件所在的目录
-#     directory = os.path.join(STATIC_DIR, os.path.dirname(filename))
-#
-#     # 返回文件
-#     print(directory)
-#     print(os.path.basename(filename))
-#     return send_from_directory(os.path.dirname(filename), os.path.basename(filename))
 
 
 @api.route('/static/<path:filename>')
 def static_file(filename):
+    """
+    发送静态文件接口，根据请求中的文件名返回相应的静态文件。
+
+    ### 参数
+    | 参数名       | 必填 | 请求类型 | 类型   | 说明                           |
+    |--------------|------|----------|--------|--------------------------------|
+    | filename     | 是   | path     | str    | 文件名                         |
+
+    ### 请求示例
+    - GET 请求，URL 中包含文件名。
+    - 例如：`http://localhost:5000/api/static/three_channel/ID_0188_Z_0137.png`
+
+    ### 返回
+    - 返回指定的静态文件。
+    - 如果文件不存在，返回 404 错误。
+    """
     print("发送图片: " + filename)
     return send_from_directory(STATIC_DIR, filename)
--- back_end/api/workspace.py
+++ back_end/api/workspace.py
@@ -21,55 +21,42 @@ from back_end.util.clear_results import clear_results
 from back_end.util.post_process import post_process_image, process_image, deeplab_postprocess
 
 
-# @api.route("upload", methods=["POST"])
-# def upload():
-#     """
-#     上传文件接口，用于接收前端上传的图像文件，并根据图像的通道数保存为不同的路径。
-#
-#     ### args
-#     |  args          | required | request type | type |  remarks                  |
-#     |----------------|----------|--------------|------|---------------------------|
-#     | file           |  true    |    form-data | file | 需要上传的图像文件  |
-#
-#     ### request
-#     - 表单数据，包含一个名为 `file` 的字段，用于上传图像文件。
-#
-#     ### return
-#     ```
-# json
-#     {
-#         "message": "File uploaded successfully"
-#     }
-#     ```
-#     - 成功上传文件后返回的消息。
-#     - 如果请求中没有文件部分，则返回错误信息和状态码 400。
-#     """
-#     if 'file' not in request.files:
-#         return jsonify({'error': 'No file part in the request'}), 400
-#     file = request.files['file']
-#     original_img_path = os.path.join(ORIGINAL_DIR, file.filename)
-#     one_channel_path = os.path.join(ONE_CHANNEL_DIR, file.filename)
-#     three_channel_path = os.path.join(THREE_CHANNEL_DIR, file.filename)
-#     file.save(original_img_path)
-#     image = Image.open(original_img_path)
-#     if len(image.split()) == 1:
-#         shutil.copy2(original_img_path, one_channel_path)
-#         img_gray = cv2.imread(original_img_path, cv2.IMREAD_GRAYSCALE)
-#         img_rgb = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
-#         cv2.imwrite(three_channel_path, img_rgb)
-#     else:
-#         shutil.copy2(original_img_path, three_channel_path)
-#         img = cv2.imread(original_img_path, cv2.IMREAD_GRAYSCALE)
-#         cv2.imwrite(one_channel_path, img)
-#     return jsonify({'message': 'File uploaded successfully'}), 200
 @api.route("/upload", methods=["POST"])
 def upload():
     """
     上传文件接口
-    - 请求参数: 表单数据: files: 文件列表
-    - 成功响应: HTTP 状态码 200
+
+    ### 参数
+    | 参数名       | 必填 | 请求类型 | 类型   | 说明                           |
+    |--------------|------|----------|--------|--------------------------------|
+    | files        | 是   | form-data | list   | 文件列表                       |
+
+    ### 请求示例
+    ```
+bash
+    curl -X POST http://localhost:5000/api/upload -F "files=@/path/to/image1.png" -F "files=@/path/to/image2.png"
+    ```
+    ### 成功响应
+    - HTTP 状态码 200
     - 响应体（JSON 格式）: 包含上传文件信息的 JSON 数组
-    - 失败响应: HTTP 状态码 500
+    ```
+json
+    [
+        {
+            "name": "image1.png",
+            "url": "http://localhost:5000/api/static/uploaded/three_channel/image1.png"
+        },
+        {
+            "name": "image2.png",
+            "url": "http://localhost:5000/api/static/uploaded/three_channel/image2.png"
+        }
+    ]
+    ```
+    ### 失败响应
+    - HTTP 状态码 400
+    - 响应体（JSON 格式）: {"status": "error", "message": "No file part in the request"}
+
+    - HTTP 状态码 500
     - 响应体（JSON 格式）: {"status": "error", "message": "Error"}
     """
     if 'files' not in request.files:
@@ -87,37 +74,47 @@ def upload():
         image = Image.open(original_img_path)
 
         if len(image.split()) == 1:
+            print(f"{file.filename} 为单通道图片，保存三通道副本到 three_channel")
             shutil.copy2(original_img_path, one_channel_path)
             img_gray = cv2.imread(original_img_path, cv2.IMREAD_GRAYSCALE)
             img_rgb = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
             cv2.imwrite(three_channel_path, img_rgb)
         else:
+            print(f"{file.filename} 为三通道图片，保存单通道副本到 one_channel")
             shutil.copy2(original_img_path, three_channel_path)
             img = cv2.imread(original_img_path, cv2.IMREAD_GRAYSCALE)
             cv2.imwrite(one_channel_path, img)
 
         response_data.append({
             "name": file.filename,
-            "url": f'http://localhost:5000/api/static/uploaded/three_channel/'+file.filename
+            "url": f'http://localhost:5000/api/static/uploaded/three_channel/{file.filename}'
         })
 
     return jsonify(response_data), 200
 
 
-@api.route("clear", methods=["GET"])
+@api.route("/clear", methods=["GET"])
 def clear():
     """
     清理所有预测结果目录，重新创建这些目录。
 
-    ### args
+    ### 参数
     - 无
 
-    ### request
+    ### 请求示例
     - GET 请求，无需传递任何参数。
+    ```
+bash
+    curl -X GET http://localhost:5000/api/clear
+    ```
+    ### 成功响应
+    - HTTP 状态码 200
+    - 响应体（字符串）: "0"
 
-    ### return
-    - 成功清理目录后返回字符串 `"0"`。
+    ### 失败响应
     - 如果在清理过程中发生错误，会捕获异常并继续执行后续操作。
+    - HTTP 状态码 200
+    - 响应体（字符串）: "0"
     """
     try:
         shutil.rmtree(DEEPLAB_DIR)
@@ -141,33 +138,72 @@ def clear():
 def get_workspace():
     """
     获取 dataSource 数据
-    - 成功响应: HTTP 状态码 200
+
+    ### 参数
+    - 无
+
+    ### 请求示例
+    - GET 请求，无需传递任何参数。
+    ```
+bash
+    curl -X GET http://localhost:5000/api/workspace
+    ```
+    ### 成功响应
+    - HTTP 状态码 200
     - 响应体（JSON 格式）: 包含工作区数据的 JSON 数组
-    - 失败响应: HTTP 状态码 500
+
+    ### 失败响应
+    - HTTP 状态码 500
     - 响应体（JSON 格式）: 空数组
     """
-    # try:
-    with open(WORKSPACE_PATH, 'r', encoding='utf-8') as f:
-        data_source = json.load(f)
-    return jsonify(data_source), 200
-    # except Exception as e:
-    #     return jsonify([]), 500
+    try:
+        with open(WORKSPACE_PATH, 'r', encoding='utf-8') as f:
+            data_source = json.load(f)
+        return jsonify(data_source), 200
+    except Exception as e:
+        return jsonify([]), 500
 
 
 @api.route("/update-workspace", methods=["POST"])
 def update_workspace():
     """
     更新 workspace 数据
-    - 请求参数: 包含工作区数据的 JSON 数组
-    - 成功响应: HTTP 状态码 200
+
+    ### 参数
+    | 参数名       | 必填 | 请求类型 | 类型   | 说明                           |
+    |--------------|------|----------|--------|--------------------------------|
+    | data         | 是   | json     | list   | 包含工作区数据的 JSON 数组     |
+
+    ### 请求示例
+    ```
+json
+    {
+        "data": [
+            {
+                "id": 1,
+                "name": "Image1",
+                "url": "http://localhost:5000/api/static/uploaded/three_channel/image1.png"
+            },
+            {
+                "id": 2,
+                "name": "Image2",
+                "url": "http://localhost:5000/api/static/uploaded/three_channel/image2.png"
+            }
+        ]
+    }
+    ```
+    ### 成功响应
+    - HTTP 状态码 200
     - 响应体（JSON 格式）: {"status": "success"}
-    - 失败响应: HTTP 状态码 500
+
+    ### 失败响应
+    - HTTP 状态码 500
     - 响应体（JSON 格式）: {"status": "error", "message": "Error details"}
     """
     try:
         data = request.json
         with open(WORKSPACE_PATH, 'w', encoding='utf-8') as f:
-            json.dump(data, f,ensure_ascii=False)
+            json.dump(data, f, ensure_ascii=False)
         return jsonify({"status": "success"}), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
