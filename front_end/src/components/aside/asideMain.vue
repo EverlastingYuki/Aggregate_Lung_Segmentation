55a5ec84934957f776864efff94093f43fc849a2
L <2210797528@qq.com>
2024-11-24 15:50:09 +0800

    注册路由

--- front_end/src/components/aside/asideMain.vue
+++ front_end/src/components/aside/asideMain.vue
@@ -2,23 +2,30 @@
 import {ElIcon} from "element-plus";
 import {WarnTriangleFilled, Menu as IconMenu} from "@element-plus/icons-vue";
 import {useAsideStore} from "@/stores/asideStore"
+import {RouterLink} from 'vue-router'
 const { setFunction } = useAsideStore()
 </script>
 
 <template>
-  <el-menu>
-    <el-menu-item index="2" @click="setFunction('对比推理')">
-      <el-icon>
-        <icon-menu/>
-      </el-icon>
-      <template #title>对比推理</template>
-    </el-menu-item>
-    <el-menu-item index="3" @click="setFunction('历史记录')">
-      <el-icon>
-        <WarnTriangleFilled/>
-      </el-icon>
-      <template #title>历史记录</template>
-    </el-menu-item>
+  <el-menu :default-active="'2'">
+    <RouterLink to="/inferenceMain">
+      <el-menu-item index="2" @click="setFunction('对比推理')">
+        <el-icon>
+          <icon-menu/>
+        </el-icon>
+        <template #title>对比推理</template>
+      </el-menu-item>
+    </RouterLink>
+
+    <RouterLink to="/historyMain">
+      <el-menu-item index="3" @click="setFunction('历史记录')">
+        <el-icon>
+          <WarnTriangleFilled/>
+        </el-icon>
+        <template #title>历史记录</template>
+      </el-menu-item>
+    </RouterLink>
+
     <el-menu-item index="4" disabled>
       <el-icon>
         <WarnTriangleFilled/>
@@ -29,5 +36,7 @@ const { setFunction } = useAsideStore()
 </template>
 
 <style scoped>
-
+  a {
+  text-decoration: none;
+}
 </style>
